3d6d54043e46eeebe852c5aa3e8a01051b6e55b4
Kevin Peters <kwpeters@ra.rockwell.com>
2016-03-06 20:27:54 -0500

    Added buildTypeScript() and exec().

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-var Promise     = require("es6-promise").Promise,
-    consume     = require("stream-consume"),
-    mergeStream = require("merge-stream");
+/* global gulp */
+
+var Promise     = require("es6-promise").Promise;
 
 
-function streamsToPromise() {
-    var streams = Array.prototype.slice.call(arguments);
+/**
+ * Converts a variable number of readable streams into a promise.
+ * @param {...Readable} var_args - Readable streams that will be merged
+ * @returns {Promise} A promise that will be resolved with undefined when all
+ * of the streams have ended.  If any of the streams emit an "error" event, the
+ * error will be logged on the console and the promise will be rejected with
+ * that error.
+ */
+function streamsToPromise(var_args) { //eslint-disable-line no-unused-vars
+    var mergeStream = require("merge-stream"),
+        consume     = require("stream-consume"),
+        streams     = Array.prototype.slice.call(arguments);
 
     return new Promise(function (resolve, reject) {
 
@@ -25,13 +35,81 @@ function streamsToPromise() {
             resolve();
         });
 
+        // Workaround for issue where readable streams are not processed
+        // unless there is a consumer.
         consume(merged);
     });
+}
 
 
+function buildTypeScript(srcGlobs, jsOutputDir, typingsOutputDir) {
+    "use strict";
+
+    var ts                   = require("gulp-typescript"),
+        sourcemaps           = require("gulp-sourcemaps"),
+        emitDeclarationFiles = !!typingsOutputDir,
+        tsResults,
+        streams              = [];
+
+    tsResults = gulp
+        .src(srcGlobs /*, {base: 'src'}*/)
+        .pipe(sourcemaps.init())
+        .pipe(ts(
+            {
+                target:            "ES5",
+                declarationFiles:  emitDeclarationFiles,
+                noExternalResolve: false,
+                noEmitOnError:     true,
+                module:            "commonjs"
+            },
+            undefined,
+            ts.reporter.longReporter())
+        );
+
+    // Add the js stream.
+    streams.push(tsResults.js
+                     .pipe(sourcemaps.write())
+                     .pipe(gulp.dest(jsOutputDir)));
+
+    // If creating declaration files, prepare that stream.
+    if (emitDeclarationFiles) {
+        streams.push(tsResults.dts
+                         .pipe(gulp.dest(typingsOutputDir)));
+    }
+
+    streamsToPromise.apply(null, streams);
+}
+
+
+// Just like Node's child_process.exec(), but returns a promise.  The promise is
+// fulfilled (with stdout) when the process exits successfully.  The promise is
+// rejected with an Error object otherwise.
+function exec(command, options) {
+    "use strict";
+
+    return new Promise(function (resolve, reject) {
+        var nodeExec = require("child_process").exec;
+
+        nodeExec(
+            command,
+            options,
+            function (err, stdout, stderr) {
+                if (err) {
+                    console.log(stderr);
+                    reject(err);
+                    return;
+                }
+
+                console.log(stdout);
+                resolve(stdout);
+            }
+        );
+    });
 }
 
 
 module.exports = {
-    streamsToPromise: streamsToPromise
+    streamsToPromise: streamsToPromise,
+    buildTypeScript:  buildTypeScript,
+    exec:             exec
 };
